c9a41557aaf05ace81f2c840944ab3c0e49f83df
Claus-Justus Heine <himself@claus-justus-heine.de>
2024-01-27 16:57:38 +0100

    FilePrefixPicker: display untruncated file-name as popup.

--- lib/components/FilePrefixPicker.vue
+++ lib/components/FilePrefixPicker.vue
@@ -24,7 +24,8 @@
       {{ hint }}
     </div>
     <div class="flex flex-center flex-wrap">
-      <div class="dirname">
+      <div v-tooltip="unclippedPopup(pathInfo.dirName)"
+           class="dirname">
         <a href="#"
            class="file-picker button icon-folder"
            @click.prevent.stop="!disabled && openFilePicker(...arguments)"
@@ -34,6 +35,7 @@
       </div>
       <SettingsInputText v-if="!onlyDirName"
                          v-model="pathInfo.baseName"
+                         v-tooltip="unclippedPopup(pathInfo.baseName)"
                          label=""
                          class="flex-grow"
                          :placeholder="placeholder"
@@ -164,9 +166,26 @@ export default {
         }
       }
     },
+    unclippedPopup(content, html) {
+      return {
+        content,
+        preventOverflow: true,
+        html: true,
+        // shown: true,
+        // triggers: [],
+        csstag: ['vue-tooltip-unclipped-popup'],
+      };
+    },
   },
 }
 </script>
+<style lang="scss">
+[csstag="vue-tooltip-unclipped-popup"].v-popper--theme-tooltip {
+  .v-popper__inner {
+    max-width:unset!important;
+  }
+}
+</style>
 <style lang="scss" scoped>
 .input-wrapper {
   .dirname {
